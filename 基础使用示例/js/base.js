eac4908341e7c8c435d56c18d0bd354011cda1b9
aulence <aulence@foxmail.com>
2018-01-14 23:08:46 +0800

    新增忽略文件

--- 基础使用示例/js/base.js
+++ 基础使用示例/js/base.js
@@ -6,14 +6,14 @@
 let app1 = new Vue({
 	el: '#app-1',
 	data: {
-		message: '#app-1：这是我希望在该标签输出的内容'
+		message: '这是我希望在该标签输出的内容'
 	}
 });
 
 let app2 = new Vue({
 	el: '#app-2',
 	data: {
-		title: '#app-2：鼠标悬浮提示文本'
+		title: '这是鼠标悬浮后提示的文本'
 	}
 });
 
@@ -28,27 +28,13 @@ let app3 = new Vue({
 let app4 = new Vue({
 	el: '#app-4',
 	data: {
+        // 汽车信息
 		carInfo: [
-			{
-				name: '柯尼塞格one1',
-				price: '99999999'
-			},
-			{
-				name: 'LykanHypersport',
-				price: '90000000'
-			},
-			{
-				name: '迈巴赫exelero',
-				price: '60000000'
-			},
-			{
-				name: '西尔贝',
-				price: '50000000'
-			},
-			{
-				name: '阿斯顿马丁one-77',
-				price: '47000000'
-			}
+			{ name: '柯尼塞格one1', price: '99999999' },
+			{ name: 'LykanHypersport', price: '90000000' },
+			{ name: '迈巴赫exelero', price: '60000000' },
+			{ name: '西尔贝', price: '50000000' },
+			{ name: '阿斯顿马丁one-77', price: '47000000' }
 		]
 	}
 });
@@ -57,10 +43,18 @@ let app5 = new Vue({
 	el: '#app-5',
 	data: {
 		content: '#app-5：原来的文本内容。'
-	},
+    },
+    // Vue的事件都是通过这个对象进行设置的
 	methods: {
+        // 添加文本的事件
 		addText: function () {
-			this.content += `新增的文本内容`;
+            const testStr = '新增的文本内容';
+            // 如果找不到对应的字符串，则新增指定文本
+            if(!this.content.includes(testStr)) {
+                this.content += testStr;
+            } else {
+               alert('文本已经添加！');
+            }
 		}
 	}
 });
