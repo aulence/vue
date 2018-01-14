9ffae30b38faeda6cd1f4e3ff30bdde4aad1287e
aulence <aulence@foxmail.com>
2018-01-15 02:28:49 +0800

    完善了Vue构造实例的内容和解释

--- 基础使用示例/js/constructor.js
+++ 基础使用示例/js/constructor.js
@@ -3,8 +3,10 @@
  * 日期：2017/4/18
  **/
 
-// 取消 Vue 所有的日志与警告
+// 关闭 Vue 所有的日志与警告
 Vue.config.silent = true;
+// 关闭 Vue 控制台中输出的“处于开发模式”的信息
+Vue.config.productionTip = false;
 
 /* 模块通用对象（全局） */
 window.appname = {
@@ -15,7 +17,7 @@ window.appname = {
 let appname = {
 	setName: '数据内容1'
 };
-let app1 = new Vue({
+const app1 = new Vue({
 	el: '#app-1',
 	data: appname
 });
@@ -25,7 +27,7 @@ let app2data = {
 	setName: '数据内容2',
 	oldVal: '--暂无--'
 };
-let app2 = new Vue({
+const app2 = new Vue({
 	el: '#app-2',
 	data: app2data
 });
@@ -33,16 +35,92 @@ app2.$watch('setName', function (newVal, oldVal) {
 	app2data.oldVal = oldVal;
 });
 
+let createData = "未处理";
 /* #app3 */
-let app3 = new Vue({
-	el: '#app3',
+const app3 = new Vue({
+	el: '#app-3',
 	data: {
+        handle: createData,
 		name: '我是app-3的名称',
 		prop: '我是app-3的属性'
-	},
-	// hook
+    },
+    // 钩子函数：数据实例创建前执行
+    beforeCreate: function() {
+        createData = "处理后";
+    },
+	// 钩子函数：数据实例创建后执行
 	created: function () {
-		document.getElementsByClassName('content')[0].textContent = `${this.name}，${this.prop}。`;
+        document.getElementsByClassName('content')[0].textContent = `${this.handle}： ${this.name}，${this.prop}。`;
 	}
+});
+
+/* #app4 */
+const app4 = new Vue({
+    el: '#app-4',
+    data: {
+        testString: ''
+    },
+    created: function () {
+        this.testString = '测试文本内容';
+    },
+    // 钩子函数：模板编译之后，但在VM实例替换视图前执行
+    beforeMount: function() {
+        this.testString = '被修改后的' + this.testString;
+    },
+	// 钩子函数：VM实例替换视图后执行
+	mounted: function () {
+        this.testString += "~~~"
+        this.$refs.hookElement.style.cssText = `
+            padding: 10px 12px;
+            background-image: linear-gradient(179deg, rgb(222, 108, 251), rgb(83, 3, 103));
+            color:#fff;
+        `;
+    }
+});
+
+/* #app5 */
+const app5 = new Vue({
+    el: '#app-5',
+    data: {
+        testString: '测试的',
+        run: false,
+    },
+	mounted: function () {
+        this.testString += "文本内容"
+    },
+    // 钩子函数：数据更新前执行
+    beforeUpdate: function() {
+        if(this.testString.includes('文本内容')) {
+            this.run = true;
+        }
+    },
+    // 钩子函数：数据更新后执行
+    updated: function() {
+        if(this.run) {
+            this.$refs.hookElement.style.fontSize = "32px";
+        } else {
+            alert('请确保数据中包含“文本内容”字样！');
+        }
+    }
+});
+
+/* #app6 */
+const app6 = new Vue({
+    el: '#app-6',
+    data: {
+        str: '我想要被销毁'
+    },
+    methods: {
+        deleteVM() {
+            app6.$destroy();
+        }
+    },
+    beforeDestroy: function() {
+        this.$refs.hookElement.style.color = '#f00';
+        alert('我可能要被销毁了');
+    },
+    destroyed: function() {
+        alert('我已经被销毁了。无法再对“app6”这个VM进行修改了。');
+    }
 });
 
