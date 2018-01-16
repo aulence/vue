0cf8866c8aeea2b0f44a40bab5f1c4856113956e
aulence <aulence@foxmail.com>
2018-01-17 00:25:32 +0800

    完善了Vue过滤器一章的内容

--- 基础使用示例/js/filter.js
+++ 基础使用示例/js/filter.js
@@ -8,30 +8,52 @@
 /*******************************************/
 window.onload = function () {
 	
-	let app1 = new Vue({
+	const app1 = new Vue({
 		el: '#app-1',
 		data: {
 			content: 'this is vue filter!'
 		}
 	});
 	
-	let app2 = new Vue({
+	const app2 = new Vue({
 		el: '#app-2',
 		data: {
-			money: 234659.34565
+			money: 234659.3
 		}
 	});
 	
-	let app3 = new Vue({
+	const app3 = new Vue({
 		el: '#app-3',
 		data: {
-			info: {
-				goods: '海尔智能王冰箱',
-				country: '中国',
-				price: 4999,
-				date: '2017-4-19'
+			keyboardInfo: {
+				goods: 'Cherry樱桃机械键盘-MX-BOARD 9.0',
+				country: '德国',
+				price: 1376
 			}
 		}
+    });
+    
+    const app4 = new Vue({
+		el: '#app-4',
+		data: {
+			refrigerator: {
+				goods: '海尔智能王冰箱',
+				country: '中国',
+				manuDate: 1492531200000
+			}
+        },
+        filters: {
+            // 毫秒转本地日期格式过滤器
+            toLocalDate: function(value) {
+                if (!value) {
+                    return '';
+                }
+                if(typeof value === 'number') {
+                    value = new Date(value).toLocaleString();
+                } 
+                return value;
+            }
+        }
 	});
 };
 
@@ -41,45 +63,33 @@ window.onload = function () {
 /* 过滤器定义 */
 /*******************************************/
 // 首字母大写
-cvFilter('capitalize',function (value) {
-	value = value.toString();
-	return value.charAt(0).toUpperCase() + value.slice(1);
+Vue.filter('capitalize', function(value) {
+    if (!value) {
+        return '';
+    }
+    value = value.toString();
+    return value.charAt(0).toUpperCase() + value.slice(1);
 });
+
 // 全部字母大写
-cvFilter('uppercase',function (value) {
-	value = value.toString();
-	return value.toUpperCase();
+Vue.filter('uppercase', function(value) {
+    if (!value) {
+        return '';
+    }
+    value = value.toString();
+    return value.toUpperCase();
 });
+
 // 全部字母小写
-cvFilter('lowerCase',function (value) {
-	value = value.toString();
-	return value.toLowerCase();
-});
-// 货币符号
-cvFilter('currency',function (value) {
-	if(typeof value === 'number') {
-		value = '￥' + parseFloat(value.toFixed(2)).toLocaleString();
-		if(value.lastIndexOf('\.') === -1) {
-			value += ".00"
-		}
-	}
-	return value;
+Vue.filter('lowerCase', function(value) {
+    if (!value) {
+        return '';
+    }
+    value = value.toString();
+    return value.toLowerCase();
 });
 
-/**
- * 功能：自定义Vue过滤器
- * 参数1：过滤器名称（自定义字符串）
- * 参数2：过滤器配置函数
-**/
-function cvFilter(id, callback) {
-	Vue.filter(id, function(value) {
-		if (!value) {
-			return '';
-		}
-		return callback(value);
-	});
-}
-
+// 书名号
 Vue.filter('bookmark', function (val) {
 	if(!val) {
 		return '';
@@ -87,3 +97,30 @@ Vue.filter('bookmark', function (val) {
 	val = '《' + val + '》';
 	return val;
 });
+
+// 货币符号
+Vue.filter('currency', function(value) {
+    if (!value) {
+        return '';
+    }
+    if(typeof value === 'number') {
+        value = '￥' + parseFloat(value.toFixed(2)).toLocaleString();
+        // 如果有小数
+        if(value.lastIndexOf('\.') !== -1) {
+            // 获取小数位
+            const decimalLength = value.slice(value.lastIndexOf('\.') + 1).length;
+            // 只有有一位小数
+            if(decimalLength === 1) {
+                value += "0"
+            }
+        }
+        // 如果没有小数
+		else {
+            value += ".00"
+        }
+    }
+    return value;
+});
+
+
+
